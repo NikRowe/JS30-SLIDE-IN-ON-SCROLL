f03d74e88b63acc1ce6ce774c1e25a79fc73cb6c
Nik Rowe <NikRowe7@Gmail.com>
2020-09-29 11:05:04 -0700

    Add .active if scroll gets to 1/2 through img

--- scripts.js
+++ scripts.js
@@ -16,7 +16,23 @@ function debounce(func, wait = 20, immediate = true) {
 }
 
 function checkSlide(e) {
-    console.count(e)
+    sliderImages.forEach(sliderImg => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImg.height / 2 // gives us scroll position at bottom of page instead of top(default) then gives us scroll position of 1/2 way through Img
+        const imgBottom = sliderImg.offsetTop + sliderImg.height; // offSetTop gets the distance from the top of the page to top of the img then add the height of the Img to give the distance to bottom. 
+        const isHalfShown = slideInAt > sliderImg.offsetTop;
+        const isNotScrolledPast = window.scrollY < imgBottom;
+
+        console.log('imgBottom :', imgBottom)
+        console.log('isHalfShown :', isHalfShown)
+        console.log('isNotScrolledPast :', isNotScrolledPast)
+
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImg.classList.add('active')
+        } else {
+            sliderImg.classList.remove('active')
+        }
+
+    })
 }
 
-window.addEventListener('scroll', debounce(checkSlide, 40))
+window.addEventListener('scroll', debounce(checkSlide))
